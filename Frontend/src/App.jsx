17e2df1b3a92f6d0f38df5587cc445c86131b919
Yohan Kavishka <sdykjayathilaka@students.nsbm.ac.lk>
2025-10-26 20:58:14 +0530

    feat(created registration page)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import HeroSection from './components/layout/HeroSection'
 import GlassNavbar from './components/common/GlassNavbar'
 import './App.css'
 import JobList from './components/jobs/JobList';
+import Register from './components/auth/Register';
 
 
 const queryClient = new QueryClient();
@@ -40,10 +41,13 @@ function App() {
             <Routes>
               <Route path="/" element={<HeroSection />} />
               <Route path="/jobs" element={<JobList/>}/>
+              <Route path="/register" element={<Register />}/>
+
               
               
               
             </Routes>
+            
           </div>
         </Router>
       
